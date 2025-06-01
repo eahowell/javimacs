bd78ee617611eceb5a2f5433412783d066ddeddf
eahowell <51245710+eahowell@users.noreply.github.com>
2025-06-01 18:50:46 -0500

    refactor: moved providers to app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withDebugTracing } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withDebugTracing()),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(BrowserModule),
+    provideAnimationsAsync()
+  ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { provideRouter }        from '@angular/router';
-import { importProvidersFrom }  from '@angular/core';
-import { BrowserModule }        from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { routes }       from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig).then(() => {
-  providers: [
-    importProvidersFrom(BrowserModule),
-    provideRouter(routes)
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
