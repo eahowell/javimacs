a497691d0e810096886b99712d0fcbbb2661bebc
eahowell <51245710+eahowell@users.noreply.github.com>
2025-06-02 21:20:24 -0500

    style: clean up commented out code

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,18 +1,9 @@
-// import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-// import { AsyncPipe, DOCUMENT } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-// import {
-//   MatSlideToggleChange,
-//   MatSlideToggleModule,
-// } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-// import { Observable } from 'rxjs';
-// import { map, shareReplay } from 'rxjs/operators';
-
 @Component({
   selector: 'app-toolbar',
   standalone: true,
@@ -21,7 +12,6 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     MatButtonModule,
     MatToolbarModule,
     MatTooltipModule,
-    // MatSlideToggleModule,
     RouterLink,
     RouterLinkActive,
   ],
@@ -29,16 +19,4 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './toolbar.component.scss',
 })
 export class ToolbarComponent {
-  // private breakpointObserver = inject(BreakpointObserver);
-  // private document = inject(DOCUMENT);
-
-  // isHandset$: Observable<boolean> = this.breakpointObserver
-  //   .observe(Breakpoints.Handset)
-  //   .pipe(
-  //     map((result) => result.matches),
-  //     shareReplay()
-  //   );
-  // onThemeChange(event: MatSlideToggleChange) {
-  //   this.document.body.classList.toggle('dark');
-  // }
 }
