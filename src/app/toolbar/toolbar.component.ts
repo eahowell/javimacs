469eeea59260133879d06f8e8ff1463cec1fa8c3
eahowell <51245710+eahowell@users.noreply.github.com>
2025-06-02 19:51:36 -0500

    chore(toolbar): remove dark light toggle

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,23 +1,44 @@
+// import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+// import { AsyncPipe, DOCUMENT } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+// import {
+//   MatSlideToggleChange,
+//   MatSlideToggleModule,
+// } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {AsyncPipe, DOCUMENT } from '@angular/common';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+// import { Observable } from 'rxjs';
+// import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatToolbarModule, MatTooltipModule, MatSlideToggleModule, RouterLink, RouterLinkActive],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    // MatSlideToggleModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss'
+  styleUrl: './toolbar.component.scss',
 })
 export class ToolbarComponent {
-  private document = inject(DOCUMENT);
-onThemeChange(event: MatSlideToggleChange) {
-    this.document.body.classList.toggle('dark');
-  }
+  // private breakpointObserver = inject(BreakpointObserver);
+  // private document = inject(DOCUMENT);
+
+  // isHandset$: Observable<boolean> = this.breakpointObserver
+  //   .observe(Breakpoints.Handset)
+  //   .pipe(
+  //     map((result) => result.matches),
+  //     shareReplay()
+  //   );
+  // onThemeChange(event: MatSlideToggleChange) {
+  //   this.document.body.classList.toggle('dark');
+  // }
 }
