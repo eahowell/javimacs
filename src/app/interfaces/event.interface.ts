6ab8c4e260e35d3c2c4331ad2a2117270105ddcb
eahowell <51245710+eahowell@users.noreply.github.com>
2025-07-25 18:32:09 -0500

    feat: initialize event and venue interfaces

---
+++ src/app/interfaces/event.interface.ts
@@ -0,0 +1,9 @@
+export interface Event {
+  id: string;
+  venueId: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  details: string;
+  isActive: boolean;
+}
---
+++ src/app/interfaces/venue.interface.ts
@@ -0,0 +1,10 @@
+export interface Venue {
+  id: string;
+  name: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  isActive: boolean;
+  createdDate: Date;
+}
