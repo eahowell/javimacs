332d58fa32989a41fb3485927f1572ca274d04cc
eahowell <51245710+eahowell@users.noreply.github.com>
2025-07-24 19:29:37 -0500

    build: add routing for admin page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent }    from './pages/home/home.component';
 import { MenuComponent }    from './pages/menu/menu.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { AdminComponent }   from './pages/admin/admin.component';
 
 export const routes: Routes = [
   { path: '',        component: HomeComponent, title: 'Javi Macs' },
   { path: 'menu',    component: MenuComponent, title: 'Menu | Javi Macs' },
   { path: 'contact', component: ContactComponent, title: 'Contact | Javi Macs' },
+  { path: 'admin',   component: AdminComponent, title: 'Admin | Javi Macs' },
   { path: '**',      redirectTo: '' }
 ];
 
