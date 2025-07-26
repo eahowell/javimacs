b4f65c2feb4eaec0f8321d35dd574c9033ef798b
eahowell <51245710+eahowell@users.noreply.github.com>
2025-07-26 18:06:45 -0500

    refactor: clean up unnecessary code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, ToolbarComponent],
     }).compileComponents();
   });
 
