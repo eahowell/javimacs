034b0bb700072ef3acf0d338d72811fcdd1e68d0
eahowell <51245710+eahowell@users.noreply.github.com>
2025-07-27 16:45:16 -0500

    test(app): set up basic initial testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,28 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, ToolbarComponent, RouterOutlet],
+      imports: [
+        AppComponent,
+        RouterModule.forRoot(
+        [
+          { path: '',        component: HomeComponent, title: 'Javi Macs' },
+          { path: 'menu',    component: MenuComponent, title: 'Menu | Javi Macs' },
+          { path: 'contact', component: ContactComponent, title: 'Contact | Javi Macs' },
+          { path: 'admin',   component: AdminComponent, title: 'Admin | Javi Macs' },
+          { path: '**',      redirectTo: '' }
+        ]
+      ),
+        HttpClientTestingModule
+      ],
     }).compileComponents();
   });
 
@@ -16,16 +32,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'javimacs' title`, () => {
+  it(`should have the 'Javi Mac's' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual("Javi Mac's");
   });
 
-  it('should render title', () => {
+  it('should render toolbar', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    // expect(compiled.querySelector('h1')?.textContent).toContain('Hello, javimacs');
+    expect(compiled.querySelector('app-toolbar')).toBeTruthy();
   });
 });
